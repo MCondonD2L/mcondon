4cf8a9137bcee11bfdeca37c5498ec6f327ffad4
Michael Condon <36084728+MCondonD2L@users.noreply.github.com>
2025-01-31 12:43:30 -0500

    pg17

--- headless/postgres/bds-headless-client-example-master/somePG-SQL-TableCREATES/CreateUsers.sql
+++ headless/postgres/bds-headless-client-example-master/somePG-SQL-TableCREATES/CreateUsers.sql
@@ -5,13 +5,13 @@ CREATE TABLE Users (
 	"FirstName" varchar(128) NULL,
 	"MiddleName" varchar(128) NULL,
 	"LastName" varchar(128) NULL,
-	"IsActive" bit NULL,
+	"IsActive" boolean NULL,
 	"Organization" varchar(100) NULL,
 	"ExternalEmail" varchar(512) NULL,
-	"SignupDate" timestamp(7) NULL,
-	"FirstLoginDate" timestamp(7) NULL,
+	"SignupDate" timestamp(6) NULL,
+	"FirstLoginDate" timestamp(6) NULL,
 	"Version" bigint NULL,
 	"OrgRoleId" int NOT NULL,
-	"LastAccessed" timestamp(7) NULL,
+	"LastAccessed" timestamp(6) NULL,
     CONSTRAINT PK_Users PRIMARY KEY ("UserId") 
 );
